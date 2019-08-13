2e69f55d3835f9166ed0b25c8bf2bff27bb8324b
Александр Шилов <aleksandr.schilow2012@gmail.com>
2019-08-13 23:09:27 +0300

    Update to new version of akryl-core

--- counter/src/main/kotlin/io/akryl/counter/App.kt
+++ counter/src/main/kotlin/io/akryl/counter/App.kt
@@ -1,59 +1,60 @@
 package io.akryl.counter
 
-import io.akryl.*
+import io.akryl.Component
+import io.akryl.css.css
+import io.akryl.css.em
+import io.akryl.css.px
 import io.akryl.html.Button
 import io.akryl.html.Div
 import io.akryl.html.Text
+import io.akryl.react.ReactNode
+import io.akryl.render
+import io.akryl.useReactive
 import kotlin.browser.document
 
-class Counter : StatefulWidget() {
-  override fun style() = css {
-    ".root" {
-      width = 200.px
-      margin = "auto"
-      display = "flex"
-      flexDirection = "column"
-      alignItems = "center"
-    }
-
-    ".title" {
-      textAlign = "center"
-      fontSize = 2.0.em
-    }
-
-    ".body" {
-      display = "flex"
-      alignItems = "center"
-    }
-
-    "button" {
-      width = 32.px
-      height = 32.px
-    }
-
-    ".value" {
-      margin = "0 16px"
-    }
-  }
-
-  override fun createState(context: BuildContext): State<*> = CounterState(context)
+private val root by css {
+  width(200.px)
+  margin.auto()
+  display.flex()
+  flexDirection.column()
+  alignItems.center()
 }
 
-private class CounterState(context: BuildContext) : State<Counter>(context) {
-  private var count = 0
+private val title by css {
+  textAlign.center()
+  fontSize(2.em)
+}
 
-  override fun build(context: BuildContext) = Div(clazz = "root", children = listOf(
-    Div(clazz = "title", child = Text("Counter")),
-    Div(clazz = "body", children = listOf(
-      Button(child = Text("-"), onClick = { count -= 1 }),
-      Div(clazz = "value", child = Text(count.toString())),
-      Button(child = Text("+"), onClick = { count += 1 })
+private val body by css {
+  display.flex()
+  alignItems.center()
+}
+
+private val button by css {
+  width(32.px)
+  height(32.px)
+}
+
+private val value by css {
+  margin(0.px, 16.px)
+}
+
+class Counter : Component() {
+  override fun render(): ReactNode {
+    var count by useReactive { 0 }
+
+    return  Div(clazz = root, children = listOf(
+      Div(clazz = title, child = Text("Counter")),
+      Div(clazz = body, children = listOf(
+        Button(clazz = button, child = Text("-"), onClick = { count -= 1 }),
+        Div(clazz = value, child = Text(count.toString())),
+        Button(clazz = button, child = Text("+"), onClick = { count += 1 })
+      ))
     ))
-  ))
+  }
 }
-
 
 fun main() {
   val app = document.getElementById("app")!!
-  hotMount(app, Counter())
+  render(Counter(), app)
 }
